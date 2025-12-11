065f87d51b2b09edb95f02089270ab57b4d488c9
harshrai9893 <harshofficial0509@gmail.com>
2025-12-11 23:30:36 +0530

    import change in campus.jsx

--- src/components/campus/Campus.jsx
+++ src/components/campus/Campus.jsx
@@ -4,7 +4,7 @@ import gallery_1 from '../../assets/gallery-1.png'
 import gallery_2 from '../../assets/gallery-2.png'
 import gallery_3 from '../../assets/gallery-3.png'
 import gallery_4 from '../../assets/gallery-4.png'
-import White_arrow from '../../assets/White-arrow.png'
+import White_arrow from '../../assets/white-arrow.png'
 
 
 const Campus = () => {
